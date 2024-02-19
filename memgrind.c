bc7f92956f3cf5c4c7ac0f1cff51ae542ba6bf1d
Parth-Patel0105 <69154451+Parth-Patel0105@users.noreply.github.com>
2024-02-18 19:57:59 -0500

    Update memgrind.c

--- memgrind.c
+++ memgrind.c
@@ -140,4 +140,241 @@ void test1(){
 
 }
 
+/*
+
+   Calls malloc for 1 byte and then frees the pointer immediately 3000 times.
+
+*/
+
+void test2(){
+
+    printf("TEST 2, malloc 1 byte 3000 times and free immediately:\n");
+
+    char * ptr = NULL;
+
+    int i;
+
+    int y;
+
+    int successCounter = 0;
+
+    int freeCounter = 0;
+
+    int totalMallocs = 0;
+
+    int totalFrees = 0;
+
+    long avg= 0;
+
+    struct timeval tv1;
+
+    struct timeval tv2;
+
+    //time_t begin;
+
+    //time_t currtime;
+
+    for (y=0; y<100; y++) {
+
+  
+
+        gettimeofday(&tv1, 0);
+
+        for (i=0; i<3000; i++) {
+
+            ptr = malloc(1);
+
+            totalMallocs++;
+
+            if (ptr != NULL){
+
+                 free(ptr);
+
+                 successCounter++;
+
+                 freeCounter++;
+
+                 totalFrees++;
+
+            }
+
+        }
+
+        gettimeofday(&tv2, 0);
+
+        avg+=((tv2.tv_sec-tv1.tv_sec)*1000000 + tv2.tv_usec-tv1.tv_usec);
+
+    }
+
+    char * test = malloc(4998);
+
+    printf("Total mallocs: %d\n", totalMallocs);
+
+    printf("Successful mallocs: %d\n", successCounter);
+
+    printf("Total frees: %d\n", totalFrees);
+
+    printf("Successful frees: %d\n", freeCounter);
+
+    printf("AVG Time Elasped: %ld microseconds.\n", avg/100);
+
+    if (test != NULL){
+
+       printf("TEST PASSED\n");
+
+    }
+
+    free(test);
+
+}
+
+void test3(){
+
+    //int i;
+
+    int mallocCounter = 0;
+
+    int freeCounter = 0;
+
+    int totalMallocs = 0;
+
+    int successCounter = 0;
+
+    char * pointers[3000];
+
+    int length = 0;
+
+    long avg= 0;
+
+  
+
+    struct timeval tv1;
+
+    struct timeval tv2;
+
+    //time_t t;
+
+    //time_t begin;
+
+    //time_t currtime;
+
+    printf("TEST 3, Randomly choose between a malloc() of size 1 or free() 6000 times:\n");
+
+    //srand((unsigned)time(&t));
+
+    int y;
+
+    for (y = 0; y < 100; y++){
+
+       gettimeofday(&tv1, 0);
+
+        while(mallocCounter < 3000){
+
+            int pick = rand_lim(0, 1);
+
+            if (pick == 0){
+
+                int size = 1;       //random size
+
+                void * ptr = malloc(size);
+
+                totalMallocs++;
+
+                if (ptr != NULL){
+
+                    //printf("pointer is stored\n");
+
+                    pointers[length] = ptr;
+
+                    length++;
+
+                    //printf("length: %d\n", length);
+
+                    successCounter++;
+
+                }
+
+                mallocCounter++;
+
+             
+
+            }
+
+            else {
+
+                //free random pointer
+
+                if (length > 0){
+
+                   int index = rand_lim(0, length - 1);
+
+                   free(pointers[index]);
+
+                   char ** start = &pointers[0];
+
+                      deleteArrayItem(index, length, start);
+
+                    length--;
+
+                    freeCounter++;
+
+                }
+
+            }
+
+      
+
+        }
+
+   //if not enough frees called
+
+        while (length > 0){
+
+            int index = rand_lim(0, length - 1);
+
+            free(pointers[index]);
+
+            char ** start = &pointers[0];
+
+            deleteArrayItem(index, length, start);
+
+            length--;
+
+            freeCounter++;
+
+            //printf("length: %d\n", length);
+
+        }
+
+        mallocCounter = 0;
+
+        gettimeofday(&tv2, 0);
+
+        avg += ((tv2.tv_sec-tv1.tv_sec)*1000000 + tv2.tv_usec-tv1.tv_usec);
+
+    }
+
+  
+
+    printf("Total mallocs: %d\n", totalMallocs);
+
+    printf("Successful mallocs: %d\n", successCounter);
+
+    printf("Frees: %d\n", freeCounter);
+
+    printf("Avg Time Elasped: %ld microseconds.\n", avg/100);
+
+    char * test = malloc(4998);
+
+    if (test != NULL){
+
+       printf("TEST PASSED\n");
+
+    }
+
+    free(test);
+
+}
+/*test 4 next*/
+
 /* to be continued... */
